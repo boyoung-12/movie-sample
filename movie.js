fc72347b624b2daddaebb3c72f650af481ecc50a
boyoung-12 <boyoung182@gmail.com>
2024-07-29 13:14:45 +0900

    개인과제1

---
+++ movie.js
@@ -0,0 +1,48 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2JiNGJhZDg4YmQ1NDNlNmRmZWU3OGI2MTI2ODM1ZSIsIm5iZiI6MTcyMjE3OTU1Mi41NzI2MTksInN1YiI6IjY2YTM3Y2U1M2RlMjEwMjExMjA4ZjJjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V6YPXswoWlGluDoxMvsRIuVBINEXtUzUDpEKfe8IQ1c'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      const movieContainer = document.getElementById('movie-container');
+      console.log(movieContainer);
+      movies.forEach(movie => {
+        const card = createMovieCard(movie);
+        movieContainer.appendChild(card);
+      });
+    })
+    .catch(err => console.error(err));
+
+    function createMovieCard(movie) {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+        <img class="card-image"src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        <h3>${movie.title}</h3>
+        <p>${movie.overview}</p>
+        <span>Rating: ${movie.vote_average}</span>
+      `;
+      card.addEventListener('click', () => alert(`Movie ID: ${movie.id}`));
+      return card;
+    }
+
+
+    document.getElementById('search-button').addEventListener('click', () => {
+      const query = document.getElementById('search-input').value.toLowerCase();
+      const movieCards = document.querySelectorAll('.movie-card');
+      movieCards.forEach(card => {
+        const title = card.querySelector('h3').textContent.toLowerCase();
+        if (title.includes(query)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+    
